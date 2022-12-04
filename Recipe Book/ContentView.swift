82bc18509d1dd4c0d663a8c36100f69b8fbb71c6
alexgonzdev <alexgonzdev@gmail.com>
2022-12-04 08:20:24 -0400

    Initial Commit

---
+++ Recipe Book/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Recipe Book
+//
+//  Created by Alex Gonzalez on 12/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recipe Book/Recipe_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_BookApp.swift
+//  Recipe Book
+//
+//  Created by Alex Gonzalez on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
